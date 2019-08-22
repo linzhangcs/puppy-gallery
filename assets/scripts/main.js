4344655d0fbf41f627574aa451d734b176c91cab
Lin Zhang <lin.zhang.ux@gmail.com>
2019-08-22 09:08:41 -0700

    update for displaying thumbnail images to the page

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -8,40 +8,65 @@
  * param
  * return
  */
-var loadImageGallery = (function() {
+var thumbnailGallery = (function() {
 
-  var getData = function(filePath, callback) {
+  var init = function(filePath) {
+    var data;
+    getImageData(filePath, displayThumbnails)
+    return data;
+  };
+
+  var getImageData = function(filePath, callback) {
     var request = new XMLHttpRequest();
     request.overrideMimeType('application/json');
 
     request.open('GET', filePath, true);
     request.onreadystatechange = function() {
-      if (request.readyState == 4 && request.status == '200') {
-        callback(request.responseText);
+      if (this.readyState == 4 && this.status == '200') {
+        callback(this.responseText);
       }
     }
     request.send(null);
   };
 
-  var parseData = function(filePath) {
-    var data;
-    getData(filePath, function(response) {
-      data = JSON.parse(response);
-      console.table(data);
-    })
-    return data;
+  var createImgElm = function(src, alt, title) {
+    var img = document.createElement('img');
+    img.src = src;
+    img.width = 300;
+    img.height = 400;
+    if (alt != null) {
+      img.alt = alt;
+    }
+    if (title != null) {
+      img.title = title;
+    }
+    return img;
+  };
+
+  function displayThumbnails(imageJson) {
+    data = JSON.parse(imageJson);
+    // console.table(data);
+    var items = data.dogs,
+      img, li,
+      thumbnailsElm = document.getElementById('thumbnails');
+
+    for (var i = 0; i < items.length; i++) {
+      img = createImgElm(items[i].thumbnail, '', '');
+      li = document.createElement('li');
+      li.appendChild(img);
+      thumbnailsElm.appendChild(li);
+    }
   };
 
   var sayhello = function() {
-    var msg = '(U •́ .̫ •̀ U) woof woof woof ٩(♡ε♡ )۶'
-    console.log(msg);
+    var msg = ['(U •́ .̫ •̀ U) woof', 'woof (υ◉ω◉υ) woof (υ◉ω◉υ) woof', 'o(^^ )o——–⊆^U)┬┬~… yay', 'o(･ω･｡)o—∈･^ミ┬┬~ summer', '໒( ◉ ᴥ ◉ )७ hi'];
+    console.log(msg[Math.floor(Math.random() * msg.length)]);
   };
 
   return {
     sayhello: sayhello,
-    parseData: parseData
+    init: init
   }
 }());
-
-loadImageGallery.sayhello();
-loadImageGallery.parseData('assets/data/dogs.json');
+thumbnailGallery.sayhello();
+thumbnailGallery.init('assets/data/dogs-compressed.json');
